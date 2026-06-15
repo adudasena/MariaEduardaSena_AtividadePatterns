e097aeb682e691429debb0ded5c8f52ceb888fde
Duda Sena <duda.sruiz@gmail.com>
2026-06-14 21:25:19 -0300

    feat(Personagem): prototype finalizado

--- src/main/java/criacionalPrototype/Main.java
+++ src/main/java/criacionalPrototype/Main.java
@@ -2,14 +2,14 @@ package criacionalPrototype;
 
 public class Main {
     public static void main(String[] args) {
-        Personagem guerreiroBase = new Personagem("Guerreiro Base", "Guerreiro", 1, 100, 15);
+        Personagem guerreiroBase = new Personagem("Guerreiro Base", "Guerreiro", 3, 500, 200);
 
         // exibição
         System.out.println("=== Personagem Base ===");
         System.out.println(guerreiroBase);
         System.out.println();
 
-        // clonagem dos 3 personagens
+        // clonagem dos personagens
         Personagem clone1 = guerreiroBase.clone();
         clone1.setNome("Vieira");
 
@@ -20,12 +20,17 @@ public class Main {
         clone3.setNome("Loki");
 
         Personagem clone4 = guerreiroBase.clone();
-        clone3.setNome("Iron");
+        clone4.setNome("Iron");
+
+        Personagem clone5= guerreiroBase.clone();
+        clone5.setNome("Lima");
 
         // exibir todos os clonados
         System.out.println("=== Personagens Clonados ===");
         System.out.println(clone1);
         System.out.println(clone2);
         System.out.println(clone3);
+        System.out.println(clone4);
+        System.out.println(clone5);
     }
 }
