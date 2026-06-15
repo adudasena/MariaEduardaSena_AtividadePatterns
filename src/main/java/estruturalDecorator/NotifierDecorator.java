9b662edf56420acfb40988d9cfa1f9054deb26d4
Duda Sena <duda.sruiz@gmail.com>
2026-06-14 21:43:22 -0300

    feat(NotifierDecorator): classe abstrata referenciando o objeto envelopado

---
+++ src/main/java/estruturalDecorator/NotifierDecorator.java
@@ -0,0 +1,14 @@
+package estruturalDecorator;
+
+public abstract class NotifierDecorator implements Notifier {
+    protected final Notifier wrappee;
+
+    public NotifierDecorator(Notifier wrappee) {
+        this.wrappee = wrappee;
+    }
+
+    @Override
+    public void send(String message) {
+        wrappee.send(message);
+    }
+}
