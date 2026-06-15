e51d70074ce04ea12e78f755abdfa47250e3b5ea
Duda Sena <duda.sruiz@gmail.com>
2026-06-14 21:07:06 -0300

    feat(Personagem): construtor do personagem

--- src/main/java/criacionalPrototype/Personagem.java
+++ src/main/java/criacionalPrototype/Personagem.java
@@ -1,4 +1,17 @@
 package criacionalPrototype;
 
-public class Personagem {
+public class Personagem implements Cloneable {
+    private String nome;
+    private String classe;
+    private int nivel;
+    private int vida;
+    private int ataque;
+
+    public Personagem(String nome, String classe, int nivel, int vida, int ataque) {
+        this.nome = nome;
+        this.classe = classe;
+        this.nivel = nivel;
+        this.vida = vida;
+        this.ataque = ataque;
+    }
 }
