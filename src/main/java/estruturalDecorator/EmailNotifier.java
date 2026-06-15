61015780748905659e4de74402fc6ef65d7e23a8
Duda Sena <duda.sruiz@gmail.com>
2026-06-14 21:34:05 -0300

    feat(EmailNotifier): classe que implementa o notifier criada

---
+++ src/main/java/estruturalDecorator/EmailNotifier.java
@@ -0,0 +1,8 @@
+package estruturalDecorator;
+
+public class EmailNotifier implements Notifier {
+    @Override
+    public void send(String message) {
+        System.out.println("Enviando EMAIL: " + message);
+    }
+}
